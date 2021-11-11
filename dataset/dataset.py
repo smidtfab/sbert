c53a8a2eb4397b8362ada73102e3022c0ce689d2
Fabian Schmidt <fabian.schmidt@fokus.fraunhofer.de>
2021-11-11 17:20:51 +0100

    Add comments

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -6,12 +6,25 @@ class Dataset(Dataset):
     The dataset used for training the Network. An element
     contains id, text, label
     """
-    def __init__(self, training_tsv_path=None, transform=None):
+    def __init__(self, training_tsv_path, transform=None):
+        """Init for the custom dataset. Read the data.
 
+        Args:
+            training_tsv_path (string): the path to the tsv
+            transform (torch.transform, optional): the transformers applied to the data. Defaults to None.
+        """
         self.train_df = reader.read_tsv(training_tsv_path)  
         self.transform = transform
 
     def __getitem__(self, index):
+        """Get an item at an index.
+
+        Args:
+            index (int): the index
+
+        Returns:
+            list: list containing the two sentences and label
+        """
        
         # Get the data
         item = self.train_df[index]
@@ -23,4 +36,9 @@ class Dataset(Dataset):
         return item
 
     def __len__(self):
+        """Get the length of the dataset
+
+        Returns:
+            int: lenght of the dataset
+        """
         return len(self.train_df)
