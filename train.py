f146907da05157f471fab3b13602f038eb1b9efa
Fabian Schmidt <fabian.schmidt@fokus.fraunhofer.de>
2021-11-12 00:26:28 +0100

    Add few comments

--- train.py
+++ train.py
@@ -6,7 +6,8 @@ from torch.utils.data import DataLoader
 from utils.load_settings import load_settings
 
 
-def main():    
+def main():
+    # Parse args that have been provided    
     parser = argparse.ArgumentParser(description="Training Script")
     parser.add_argument('--settings_path', help='path to settings')
     args = parser.parse_args()
@@ -18,12 +19,16 @@ def main():
     else:
         path_settings = os.path.join(os.path.dirname(__file__), 'settings.json')
 
+    # Load settings from json
     settings = load_settings(path_settings)
     print(settings)
 
+    # Load the data
     train_set = Dataset(settings['data']['training_data_path'])
     train_loader = DataLoader(train_set, batch_size = settings['network']['batch_size'], shuffle=True)
     print(len(train_loader))
+
+    # Get BERT model 
 
 if __name__ == "__main__":
     main()
