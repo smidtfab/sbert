20d59f01c5474a6e22ef1488ccb68614650b6cfa
Fabian Schmidt <fabian.schmidt@fokus.fraunhofer.de>
2021-11-14 21:34:18 +0100

    Add comments for Base_Transformer

--- model/Base_Transformer.py
+++ model/Base_Transformer.py
@@ -1,32 +1,37 @@
+from pickle import DICT
+from typing import Dict
 import torch
 from torch import nn
 import tqdm
 import numpy as np
 from transformers import AutoTokenizer, AutoModel
 class Transformer(nn.Module):
-    def __init__(self, model_name, tokenizer_name, batch_size=16) -> None:
+    def __init__(self, model_name, tokenizer_name, batch_size=16):
+        """Init the base transfomer model
+
+        Args:
+            model_name (str): the model name. Used to get the pretrained model (here bert)
+            tokenizer_name (str): the tokenizer name. Used to get the tokenizer (here bert)
+            batch_size (int, optional): the batch size. Defaults to 16.
+        """
         super().__init__()
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         self.model_name = model_name
         self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
-        self.transformer = AutoModel.from_pretrained(model_name)
+        self.transformer = AutoModel.from_pretrained(model_name).to(self.device)
         self.batch_size = batch_size
-        self.device = 'gpu' if torch.cuda.is_available() else 'cpu'
 
-    def forward(self, sentence_pair_list):
-        tokenizer_output = self.tokenizer(sentence_pair_list, max_length=128, padding=True, truncation='longest_first', add_special_tokens=True, return_tensors='pt')
+    def forward(self, sentence_list):
+        """Base Transformer in the siamese network. Used
+        for tokenizing and transforming the input using BERT.   
+
+        Args:
+            sentence_list (list,str): a single sentence or list of sentences  
+
+        Returns:
+            [dict, torch.Tensor]: the dictionary from the tokenizer including input_ids, 
+            attention_masks and the transformer output as a tensor
+        """
+        tokenizer_output = self.tokenizer(sentence_list, max_length=128, padding=True, truncation='longest_first', add_special_tokens=True, return_tensors='pt')
         transformer_output = self.transformer(**tokenizer_output.to(self.device))
-        #print(transformer_output)
         return tokenizer_output, transformer_output
-
-if __name__ == "__main__":
-
-    text_1 = "Who was Jim Henson ?"
-    text_2 = "Jim Henson was a puppeteer"
-
-    text_3 = "This is a test"
-    text_4 = "Next sentence"
-    data = [[text_1, text_2], [text_3, text_4]]
-    with torch.no_grad():
-        model = Transformer('bert-base-cased')
-        output = model(data)
-    print(output)
