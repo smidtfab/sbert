f5ad402fd3490cbcf4b00ee6ad4b2b7238382e32
Fabian Schmidt <fabian.schmidt@fokus.fraunhofer.de>
2021-11-14 01:19:26 +0100

    Update dataset

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -1,11 +1,13 @@
 from utils import reader
 from torch.utils.data import Dataset
 
+
 class Dataset(Dataset):
     """
     The dataset used for training the Network. An element
     contains id, text, label
     """
+
     def __init__(self, training_tsv_path, transform=None):
         """Init for the custom dataset. Read the data.
 
@@ -13,7 +15,7 @@ class Dataset(Dataset):
             training_tsv_path (string): the path to the tsv
             transform (torch.transform, optional): the transformers applied to the data. Defaults to None.
         """
-        self.train_df = reader.read_tsv(training_tsv_path)  
+        self.train_df = reader.read_tsv(training_tsv_path)
         self.transform = transform
 
     def __getitem__(self, index):
@@ -25,15 +27,21 @@ class Dataset(Dataset):
         Returns:
             list: list containing the two sentences and label
         """
-       
+
         # Get the data
+        
         item = self.train_df[index]
 
-        # Apply possible transformations
-        if self.transform is not None:
-            pass
+        #sent1, sent2 = item[0], item[1]
+        sample = {}
+        sample['sent1'] = item[0]
+        sample['sent2'] = item[1]
 
-        return item
+        mapStrToInt = {"contradiction": 0, "entailment": 1, "neutral": 2}
+        sample['label'] = mapStrToInt[item[2]]
+        print(sample)
+
+        return sample['sent1'], sample['sent2'], sample['label']
 
     def __len__(self):
         """Get the length of the dataset
