58734a9964fd212a7eed8e9c0d64eefcdb2afe41
Fabian Schmidt <fabian.schmidt@fokus.fraunhofer.de>
2021-11-14 22:19:33 +0100

    Add docstring for collate

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -6,16 +6,28 @@ from typing import Dict, List, Tuple
 from utils.map_labels import mapStrToInt
 
 class CustomSentenceBatching:
+    """Custom collate function to do batching.
+    """
     def __init__(self):
         pass
 
     def __call__(
         self, batch: List[Tuple[str, str, int]]
-    ) -> Tuple[Dict[str, torch.LongTensor], Dict[str, torch.LongTensor], torch.LongTensor]:
+    ) -> Tuple[List[str, torch.LongTensor], List[str, torch.LongTensor], torch.LongTensor]:
+        """Organizes the batching
+
+        Args:
+            batch (List[Tuple[str, str, int]]): the batch as loaded in the Dataloader
+
+        Returns:
+            Tuple[Dict[str, torch.LongTensor], Dict[str, torch.LongTensor], torch.LongTensor]: the two sentences as
+            separate lists and the labels 
+        """
         x1, x2, y = zip(*batch)
         x1, x2 = list(x1), list(x2)
         y = torch.LongTensor(y)
         return x1, x2 , y
+
 class Dataset(Dataset):
     """
     The dataset used for training the Network. An element
@@ -43,7 +55,7 @@ class Dataset(Dataset):
 
         # Index the data
         item = self.train_df[index]
-        
+
         return item[0], item[1], mapStrToInt(item[2])
 
     def __len__(self):
