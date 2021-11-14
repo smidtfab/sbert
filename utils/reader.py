f46fa513f73706cc94b4ca542d51b0c27f0a3f05
Fabian Schmidt <fabian.schmidt@fokus.fraunhofer.de>
2021-11-14 14:35:49 +0100

    Add partition label to reader

--- utils/reader.py
+++ utils/reader.py
@@ -1,17 +1,16 @@
 import csv
 
-def read_tsv(file_name):
+def read_tsv(file_name, partition_label):
     rows = []
     index = 0
     with open(file_name, 'r', encoding='utf-8') as tsv:
         for line in csv.DictReader(tsv, delimiter='\t'):
-            if(line['split'] == 'train'):
+            if(line['split'] == partition_label):
                 first_sentence = line['sentence1']
                 second_sentence = line['sentence2']
                 label = line['label']
                 rows.append([first_sentence, second_sentence, label])
                 index += 1
-    print(len(rows))
     return rows
 
 if __name__ == "__main__":
