534e5313723091b850c8f26e77c359187aa3af0b
Fabian Schmidt <fabian.schmidt@fokus.fraunhofer.de>
2021-11-14 14:40:00 +0100

    Fix u and v concats

--- model/Classifier.py
+++ model/Classifier.py
@@ -8,25 +8,26 @@ class Classifier(nn.Module):
         self.num_classes = num_classes
         self.softmax_layer = nn.Linear(3 * self.sent_embedding_dim, self.num_classes)
 
-    def forward(self, pooled_sent_embeddings):
+    def forward(self, pooled_embeddings_sent1, pooled_embeddings_sent2):
         # retrieve pooled sentence input from previous model layers
-        sent1_pooled_embedding = pooled_sent_embeddings[0]
-        sent2_pooled_embedding = pooled_sent_embeddings[1]
-        print(sent1_pooled_embedding.shape)
+        #print(f"Pooled Emb 1 shape -> {pooled_embeddings_sent1.shape}")
+        #print(f"Pooled Emb 2 shape -> {pooled_embeddings_sent2.shape}")
         sent_embeddings_to_cat = []
 
         # u, v
-        sent_embeddings_to_cat.append(sent1_pooled_embedding)
-        sent_embeddings_to_cat.append(sent2_pooled_embedding)
+        sent_embeddings_to_cat.append(pooled_embeddings_sent1)
+        sent_embeddings_to_cat.append(pooled_embeddings_sent2)
 
         # |u - v|
-        abs_diff_between_sent_embeddings = torch.abs(sent1_pooled_embedding - sent2_pooled_embedding)
+        abs_diff_between_sent_embeddings = torch.abs(pooled_embeddings_sent1 - pooled_embeddings_sent2)
         sent_embeddings_to_cat.append(abs_diff_between_sent_embeddings)
 
         # (u, v, |u-v|)
-        softmax_input = torch.cat(sent_embeddings_to_cat)
-        print(softmax_input.shape)
+        softmax_input = torch.cat(sent_embeddings_to_cat, 1)
+        #print(f"Softmax input shape -> {softmax_input.shape}")
+
         # do classification
         softmax_output = self.softmax_layer(softmax_input)
-        print(softmax_output)
+        #print(f"Softmax output shape -> {softmax_output.shape}")
+        
         return softmax_output
